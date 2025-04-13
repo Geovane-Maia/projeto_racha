ebbaadf4fce53eb6ee72de189d9c1c35769be141
Geovane <dartanha272@gmal.com>
2025-04-12 23:36:40 -0300

    Criando a logica do codigo

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ const jogadores = [
     "Lucas", "Pedro", "João", "Mateus", "Gabriel",
     "Marcos", "Ricardo", "Rafael", "Thiago", "Diego"
 ];
+let time1 = [];
+let time2 = null;
+let jogadorSorteado = null;
+for (let i = 0; i < jogadores.length; i++) {
+    while(time1.length < 5 ) {
+        jogadorSorteado = Math.floor(Math.random() * jogadores.length);
+        if(!time1.includes(jogadores[jogadorSorteado]) && time1.length < 5) {
+            time1.push(jogadores[jogadorSorteado])
+        }
+        }
+    
+        console.log(time1);
+    
+    
+}
+
+
+
+
+
+//document.getElementById('sortear').addEventListener('click', () => {
+  //  console.log(jogadores);
+    
+//})
